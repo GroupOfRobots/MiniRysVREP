b71c3cd104d16c8cbc94e910cb8074ca613f3df1
Marta Pacuszka <pacuszka.m@gmail.com>
2016-07-21 14:04:21 +0200

    orientation test v2

--- robot.h
+++ robot.h
@@ -3,16 +3,32 @@
 
 #include <cmath>
 
+#ifdef DEBUG
+#include <iostream>
+#define SHOW(x) std::cout << x << std::endl;
+#else
+#define SHOW(x) 
+#endif
+
 float orientationError(float obj_x, float obj_y, float obj_or, float goal_x, float goal_y) {
 	float GoalOrientation=atan2((goal_y-obj_y),(goal_x-obj_x));
-	if (GoalOrientation-obj_or < -M_PI)
-		return 2*M_PI + (GoalOrientation - obj_or);
-	else if (GoalOrientation-obj_or > M_PI)
-		return 2*M_PI - (GoalOrientation - obj_or);
-	else
-		return GoalOrientation - obj_or;
+	float res = 0;
+	if (GoalOrientation-obj_or < -M_PI) {
+		SHOW(1)
+		res = 2*M_PI + (GoalOrientation - obj_or);
+	} else if (GoalOrientation-obj_or > M_PI) {
+		SHOW(2)
+		res = -2*M_PI + (GoalOrientation - obj_or);
+	} else {
+		SHOW(3)
+		res = GoalOrientation - obj_or;
+	}
 	
-
+#ifdef DEBUG
+	std::cout << "OR: " << obj_or << " GO: " << GoalOrientation << " ER: " << res << "\n";
+#endif
+	
+	return res;
 }
 
 #endif
